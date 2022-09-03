d2ebe99f7270cf9ed64ec41aea8bb00aeeb37f46
nick <nick@kali.nick>
2022-09-03 16:18:35 -0400

    Add input functionality

--- src/Model.java
+++ src/Model.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Model {
 
     public static void main(String[] args) {
-        String name;
+        //Input username
+        System.out.println("Enter your full name\n");
+        Scanner newScanner= new Scanner(System.in);
+        String fullName=newScanner.nextLine();
 
+        //Split full name into first and last name
+        int splitName=fullName.lastIndexOf(" ");
+        String firstName=fullName.substring(0,splitName);
+        String lastName=fullName.substring(splitName+1);
+
+        System.out.println(firstName);
 
     }
 }
